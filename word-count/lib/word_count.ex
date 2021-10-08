a5ba68d6db44749fa98d4d2a2193562c55b643c4
Dwi Prihandi <wd.irphinaid@gmail.com>
2021-10-08 10:46:59 +0700

    Exercise 1. Word Count - resolve pending test

--- word-count/lib/word_count.ex
+++ word-count/lib/word_count.ex
@@ -5,13 +5,15 @@ defmodule WordCount do
   Words are compared case-insensitively.
   """
 
-  @alphanum_splitter ~R/[^[:alnum:]\-]/u
+  @word_splitter ~R/[^[:alnum:]\-\']/u
 
   @spec count(String.t()) :: map
   def count(sentence) do
     sentence
     |> String.downcase()
-    |> String.split(@alphanum_splitter, trim: true)
+    |> String.split(@word_splitter, trim: true)
+    # remove quote on quoted words
+    |> Enum.map(&String.trim(&1, "'"))
     |> Enum.reduce(Map.new(), fn word, result ->
       Map.update(result, word, 1, &(&1 + 1))
     end)

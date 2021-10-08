0a8b6bb4e12b715f86ecffb73d1a1b8cb5abef59
Dwi Prihandi <wd.irphinaid@gmail.com>
2021-10-08 16:21:35 +0700

    Exercise 7. Flatten Array - finish

--- flatten-array/lib/flatten_array.ex
+++ flatten-array/lib/flatten_array.ex
@@ -14,5 +14,16 @@ defmodule FlattenArray do
 
   @spec flatten(list) :: list
   def flatten(list) do
+    list
+    |> flatten_list()
+    |> Enum.reject(&is_nil(&1))
   end
+
+  def flatten_list([]), do: []
+
+  def flatten_list([head | tail]) when is_list(head) do
+    flatten_list(flatten_list(head) ++ tail)
+  end
+
+  def flatten_list([head | tail]), do: [head | flatten(tail)]
 end

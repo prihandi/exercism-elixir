43bc724a2ac9dc067ea0492b3e8325413001a0c5
Dwi Prihandi <wd.irphinaid@gmail.com>
2021-10-10 08:14:32 +0700

    Exercise 8. List Ops - finish

--- list-ops/lib/list_ops.ex
+++ list-ops/lib/list_ops.ex
@@ -6,35 +6,44 @@ defmodule ListOps do
   # `++`, `--`, `hd`, `tl`, `in`, and `length`.
 
   @spec count(list) :: non_neg_integer
-  def count(l) do
-  end
+  def count(list), do: foldl(list, 0, fn _, acc -> acc + 1 end)
 
   @spec reverse(list) :: list
-  def reverse(l) do
-  end
+  def reverse(list), do: foldl(list, [], &[&1 | &2])
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-  end
+  def map(list, fun), do: foldr(list, [], &[fun.(&1) | &2])
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
+  def filter(list, fun) do
+    foldr(list, [], fn elem, acc ->
+      if fun.(elem) do
+        [elem | acc]
+      else
+        acc
+      end
+    end)
   end
 
   @type acc :: any
   @spec foldl(list, acc, (any, acc -> acc)) :: acc
-  def foldl(l, acc, f) do
-  end
+  def foldl([], acc, _), do: acc
+  def foldl([head | tail], acc, fun), do: foldl(tail, fun.(head, acc), fun)
 
   @spec foldr(list, acc, (any, acc -> acc)) :: acc
-  def foldr(l, acc, f) do
+  def foldr(list, acc, fun) do
+    list
+    |> reverse()
+    |> foldl(acc, fun)
   end
 
   @spec append(list, list) :: list
-  def append(a, b) do
-  end
+  def append(a, b), do: foldr(a, b, &[&1 | &2])
 
   @spec concat([[any]]) :: [any]
   def concat(ll) do
+    foldr(ll, [], fn list, acc ->
+      foldr(list, acc, &[&1 | &2])
+    end)
   end
 end

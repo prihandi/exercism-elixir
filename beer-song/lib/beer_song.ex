8718c6739e573ea502d95c59ba70b07b54beae02
Dwi Prihandi <dwi@prihandi.com>
2022-12-10 10:11:50 +0700

    Simplify beer-song with map_join and capitalize

--- beer-song/lib/beer_song.ex
+++ beer-song/lib/beer_song.ex
@@ -5,18 +5,15 @@ defmodule BeerSong do
   @spec verse(integer) :: String.t()
   def verse(n) do
     """
-    #{get_bottles(n)} of beer on the wall, #{get_bottles_mid(n)} of beer.
-    #{get_action_phrase(n)}, #{get_bottles_mid(n - 1)} of beer on the wall.
+    #{String.capitalize(get_bottles(n))} of beer on the wall, #{get_bottles(n)} of beer.
+    #{get_action_phrase(n)}, #{get_bottles(n - 1)} of beer on the wall.
     """
   end
 
   defp get_bottles(-1), do: get_bottles(99)
-  defp get_bottles(0), do: "No more bottles"
+  defp get_bottles(0), do: "no more bottles"
   defp get_bottles(1), do: "1 bottle"
   defp get_bottles(n), do: "#{n} bottles"
-
-  defp get_bottles_mid(0), do: String.downcase(get_bottles(0))
-  defp get_bottles_mid(n), do: get_bottles(n)
 
   defp get_action_phrase(0), do: "Go to the store and buy some more"
   defp get_action_phrase(1), do: "Take it down and pass it around"
@@ -27,8 +24,6 @@ defmodule BeerSong do
   """
   @spec lyrics(Range.t()) :: String.t()
   def lyrics(range \\ 99..0) do
-   range
-   |> Enum.map(&verse(&1))
-   |> Enum.join("\n")
+    Enum.map_join(range, "\n", &verse(&1))
   end
 end
